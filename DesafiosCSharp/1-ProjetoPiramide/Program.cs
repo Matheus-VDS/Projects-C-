076a2d58b7458d924ec0ae2a4fe38059038ed763
Matheus Vieira de Souza <101958548+Matheus-VDS@users.noreply.github.com>
2024-10-27 21:37:36 -0400

    desafios 1 e 2 concluidos

---
+++ DesafiosCSharp/1-ProjetoPiramide/Program.cs
@@ -0,0 +1,64 @@
+﻿//Autor: Matheus Vieira de Souza
+using System;
+
+class Piramide
+{
+    private int n;
+
+    public int valida
+    {
+        get 
+        { 
+            return n; 
+        }
+        set
+        {
+            if (value < 1)
+            {
+                throw new ArgumentException("O número precisa ser igual ou maior do que 1");
+            }
+            n = value;
+        }
+    }
+
+    public Piramide(int n)
+    {
+        valida = n;
+    }
+
+    // Método que desenha a pirâmide
+    public void Desenha()
+    {
+        for (int i = 1; i <= valida; i++)
+        {
+            // Imprime espaços para centralizar a pirâmide
+            Console.Write(new string(' ', valida - i));
+
+            // Imprime a primeira metade da linha
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+
+            // Imprime a segunda metade da linha
+            for (int j = i - 1; j >= 1; j--)
+            {
+                Console.Write(j);
+            }
+
+            // Pula para a próxima linha
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+    Console.Write("Informe um número: ");
+    int n = int.Parse(Console.ReadLine());
+    Piramide piramide = new Piramide(n);
+    piramide.Desenha();
+    }
+}
---
+++ DesafiosCSharp/2-Vertice/Program.cs
@@ -0,0 +1,61 @@
+﻿//Autor: Matheus Vieira de Souza
+using System;
+
+class Vertice
+{
+    public double pontoX {get; private set;} 
+    public double pontoY {get; private set;}
+
+    public Vertice(double x, double y)
+    {
+        pontoX = x;
+        pontoY = y;
+    }
+
+    public double Distancia(Vertice v1, Vertice v2)
+    {
+        double distancia_X = Math.Abs(v1.pontoX - v2.pontoX);
+        double distancia_Y = Math.Abs(v1.pontoY - v2.pontoY);
+        return Math.Sqrt(Math.Pow(distancia_X, 2) + Math.Pow(distancia_Y, 2));
+    }
+
+
+    public void Move(Vertice v) 
+    {
+        Console.Write("Informe nova posição de x para o vértice 1: ");
+        double novo_pontoX = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Informe nova posição de y para o vértice 1: ");
+        double novo_pontoY = Convert.ToDouble(Console.ReadLine());
+        Console.Write($"\nA posição do v1({v.pontoX},{v.pontoY}) agora é v1({novo_pontoX},{novo_pontoY})");
+        v.pontoX = novo_pontoX;
+        v.pontoY = novo_pontoY;
+    }
+    public void is_Equal(Vertice v1, Vertice v2)
+    {
+        if (v1.pontoX == v2.pontoX && v1.pontoY == v2.pontoY)
+            Console.Write("\nOs vértices são iguais!\n");
+        else
+            Console.Write("\nOs vértices são diferentes!\n");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe valor de x para o vértice 1: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Informe valor de y para o vértice 2: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Informe valor de x para o vértice 2: ");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Informe valor de y para o vértice 2: ");
+        double y2 = Convert.ToDouble(Console.ReadLine());
+        Vertice vertice = new Vertice(x, y);
+        Vertice vertice2 = new Vertice(x2, y2);
+
+        Console.WriteLine("\n"+vertice.Distancia(vertice, vertice2)); 
+        vertice.is_Equal(vertice, vertice2);
+        vertice.Move(vertice);
+    }
+}
